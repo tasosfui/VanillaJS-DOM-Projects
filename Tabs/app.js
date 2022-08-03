3a8783a9d969d19a00be308aec973eedbd1cd6bb
Tasos F <55960325+tasosfui@users.noreply.github.com>
2022-08-03 13:58:04 +0300

    Add files via upload

---
+++ Tabs/app.js
@@ -0,0 +1,23 @@
+const btns = document.querySelectorAll(".tab-btn");
+const about = document.querySelector(".about");
+const articles = document.querySelectorAll(".content");
+
+about.addEventListener("click", function (e) {
+  //   console.log(e.target.dataset.id);
+  const id = e.target.dataset.id;
+  if (id) {
+    // remove active class from other buttons
+    btns.forEach(function (btn) {
+      btn.classList.remove("active");
+      // add active class to the target button (clicked)
+      e.target.classList.add("active");
+    });
+    //hide other articles when button is clicked
+    articles.forEach(function (article) {
+      article.classList.remove("active");
+    });
+    // ??
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
+});
