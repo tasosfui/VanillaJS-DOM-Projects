67fbee294891e92b19704f63a639884c09ad02cf
Tasos F <55960325+tasosfui@users.noreply.github.com>
2022-09-15 15:15:52 +0300

    Update app.js

--- Modal/app.js
+++ Modal/app.js
@@ -7,8 +7,6 @@ const modalBtn = document.querySelector(".modal-btn");
 const modalOverlay = document.querySelector(".modal-overlay");
 const closeBtn = document.querySelector(".close-btn");
 
-console.log(modalBtn, modalOverlay, closeBtn);
-
 modalBtn.addEventListener("click", function () {
   modalOverlay.classList.add("open-modal");
 });
